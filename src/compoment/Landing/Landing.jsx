1554357695d786cfc437bc7f93609299ec78b8af
Jeanndo <jeanndo@pop-os.localdomain>
2021-11-27 18:12:08 +0200

    created landing page

--- src/compoment/Landing/Landing.jsx
+++ src/compoment/Landing/Landing.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import  Navigation from './LandingNavigation'
+import Banner from './Landing-page-banner'
+import LandingDescription from './Landing-Description'
 const Landing = () => {
     return (
-        <div>
-            This is The landing page
+        <div className="landing-page-container">
+           <div className="landing-page-content-container">
+                <Navigation/>
+                <Banner/>
+                <LandingDescription/>
+                <h2 style={{textAlign:'center',fontSize:'16px'}}>Powered by JEAN DE DIEU | React Dev.</h2>
+               </div>
         </div>
     )
 }
